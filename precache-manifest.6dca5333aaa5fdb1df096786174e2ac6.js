b9609bf75ad81271342876d35cc737a0a12dc96f
lbadocha <krakow@akademia108.pl>
2020-03-28 16:53:56 +0100

    Updates

---
+++ precache-manifest.6dca5333aaa5fdb1df096786174e2ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c632ddd1fba670ccf841524353f703f",
+    "url": "/beer-table/index.html"
+  },
+  {
+    "revision": "499267e51070a0d7dc2c",
+    "url": "/beer-table/static/css/main.ea114d9d.chunk.css"
+  },
+  {
+    "revision": "99cfbb8d49169e59b06e",
+    "url": "/beer-table/static/js/2.70cef6a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beer-table/static/js/2.70cef6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499267e51070a0d7dc2c",
+    "url": "/beer-table/static/js/main.94419991.chunk.js"
+  },
+  {
+    "revision": "8ccaab5abd967351bed3",
+    "url": "/beer-table/static/js/runtime-main.cdf9fe7c.js"
+  }
+]);
